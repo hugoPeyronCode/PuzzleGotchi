0b2b24261e0c5c1932927eaca10c464150e866ee
hugoPeyronCode <hugopeyron@gmail.com>
2025-12-04 23:27:29 +0100

    Initial Commit

---
+++ PuzzleGotchi/PuzzleGotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleGotchiApp.swift
+//  PuzzleGotchi
+//
+//  Created by Hugo Peyron on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleGotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
